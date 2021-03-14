ecd61d501d8a66e65f083df69a208b626a98e033
zqj5293 <80650732+zqj5293@users.noreply.github.com>
2021-03-15 11:01:19 +1300

    Created Project in NetBeans

---
+++ The Chase/src/the/chase/TheChase.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package the.chase;
+
+/**
+ *
+ * @author julia
+ */
+public class TheChase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
