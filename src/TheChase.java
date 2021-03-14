2137339c1a7e682fdd0c34e7310cc0931b99edcd
Gamma <37131904+itsnotdapro@users.noreply.github.com>
2021-03-15 11:10:53 +1300

    Update TheChase.java

--- src/TheChase.java
+++ src/TheChase.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package the.chase;
-
 /**
  *
  * @authors
  * Abby - 
  * Julia - 19078503
- * Matt - 
+ * Matt - 19076935
  */
 public class TheChase {
 
