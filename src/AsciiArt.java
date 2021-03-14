b75ea0bb9c321ad00e8084826e94fb0f2ca1093b
zqj5293 <80650732+zqj5293@users.noreply.github.com>
2021-03-15 11:33:43 +1300

    Update AsciiArt.java
    Main Ascii art board for the chase

--- src/AsciiArt.java
+++ src/AsciiArt.java
@@ -1,4 +1,25 @@
 
 public class AsciiArt {
+	
+	public void questionBoard() {
+		System.out.println("____________________________");
+		System.out.println("|                          |");
+		System.out.println("|__________________________|");
+		System.out.println(" |                        |");
+		System.out.println(" |________________________|");
+		System.out.println("  |                      |");
+		System.out.println("  |______________________|");
+		System.out.println("   |                    |");
+		System.out.println("   |____________________|");
+		System.out.println("    |                  |");
+		System.out.println("    |__________________|");
+		System.out.println("     |                |");
+		System.out.println("     |________________|");
+		System.out.println("      |              |");
+		System.out.println("      |______________|");
+		System.out.println("       |            |");
+		System.out.println("       |____________|");
+
+	}
 
 }
