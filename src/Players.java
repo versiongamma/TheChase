5a82c7fbac7690f8b47e36bc7f9e82ec6306829c
zqj5293 <80650732+zqj5293@users.noreply.github.com>
2021-03-19 16:16:55 +1300

    Update Players.java

--- src/Players.java
+++ src/Players.java
@@ -1,4 +1,31 @@
 
 public class Players {
+	
+	private String chaser;
+	private String player;
+	
+	public Players(String chaser, String player) {
+		this.chaser = chaser;
+		this.player = player;
+	}
+	
+	
+	//GETTERS AND SETTERS
+	
+	public String getChaser() {
+		return chaser;
+	}
+
+	public void setChaser(String chaser) {
+		this.chaser = chaser;
+	}
+
+	public String getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(String player) {
+		this.player = player;
+	}
 
 }
