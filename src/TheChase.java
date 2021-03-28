1ba48682611124ad26fea484a2fc07a7ab8be29f
zqj5293 <80650732+zqj5293@users.noreply.github.com>
2021-03-29 11:41:14 +1300

    Main updated

--- src/TheChase.java
+++ src/TheChase.java
@@ -1,3 +1,7 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @authors
@@ -11,7 +15,39 @@ public class TheChase {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+");
+	System.out.println("|            Welcome to              |");
+	System.out.println("|               the                  |");
+	System.out.println("|              Chase!                |");
+	System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n");
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int selection;
+        
+        do{
+            System.out.println("Please Choose an Option: \n"
+                    + "1. Play The Chase \n"
+                    + "2. How to Play \n"
+                    + "3. Exit");
+            selection = scan.nextInt();
+            
+            switch (selection) {
+                case 1:
+                    //game starts
+                    break;
+                case 2:
+                    System.out.println("Game instructions: \n"
+                            + "");
+                    break;
+                case 3:
+                    System.out.println("Goodbye!");
+                    System.exit(0);
+                    break;
+                default:
+                    throw new IndexOutOfBoundsException();
+            }
+            
+        }while(selection == 1);
     }
-    
 }
