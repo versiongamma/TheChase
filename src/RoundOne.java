5a2544647659f0573422c2e866fb8dd8572d559a
zqj5293 <80650732+zqj5293@users.noreply.github.com>
2021-04-02 11:57:56 +1300

    Update RoundOne.java

--- src/RoundOne.java
+++ src/RoundOne.java
@@ -1,3 +1,8 @@
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.util.Timer;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,4 +18,30 @@
 public class RoundOne {
     //code goes in here
     //I have found a way to make the timer work as well
+    
+    public void showQuestions(){
+        RoundOne r = new RoundOne();
+        BufferedReader br = null;
+        PrintWriter pw = null;
+        
+       for(int i = 0; i <10; i++){
+           //print questions here
+           br = new BufferedReader(new FileReader(./LongFormQuestions.txt));
+           pw = new PrintWriter("./Longformquestions.txt");
+           //print writer then buffer reader
+           
+           if(question == false){
+               System.out.println("Incorrect Answer!"
+                       + "The Answer was: ");
+               
+           }
+           else if(question == true){
+               System.out.println("Correct!");
+               r.getPlayerScore()++;
+           }
+           
+           i--;
+       }
+    }
 }
+   
