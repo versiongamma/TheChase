70015dfaaf8fb4276e8ab08a0adebbc74dbf4c0f
zqj5293 <80650732+zqj5293@users.noreply.github.com>
2021-04-07 14:09:46 +1200

    Updated main

--- src/RoundOne.java
+++ src/RoundOne.java
@@ -8,84 +8,104 @@ import java.util.TimerTask;
 
 /**
  *
- * Round One of the Chase!
- * 1 Minute timer is started - then the player will have that amount of time to
- * answer as many long answer question correctly they can in that time period
+ * Round One of the Chase! 1 Minute timer is started - then the player will have
+ * that amount of time to answer as many long answer question correctly they can
+ * in that time period
  */
-
 public class RoundOne {
-    
+
     private static String playerAnswer;
     private static Players players;
-    
-    public RoundOne(String playerAnswer, Players players){
+    Toolkit toolkit;
+    Timer timer;
+
+    /**
+     * 
+     * @param playerAnswer
+     * @param players 
+     */
+    public RoundOne(String playerAnswer, Players players) {
         RoundOne.playerAnswer = playerAnswer;
-        players = players;
+        RoundOne.players = players;
     }
-    
-    public static void printQuestion() throws IOException{
-        try{
-         System.out.println(new LongFormQuestion());
-        }catch(IOException e){
+
+    /**
+     * 
+     * @throws IOException 
+     */
+    public static void printQuestion() throws IOException {
+        try {
+            System.out.println(new LongFormQuestion());
+        } catch (IOException e) {
             System.out.println("Unable to read in question.");
         }
     }
-    
-    //checks the answer entered by the player
-    public static void checkAnswer(String playerAnswer) throws IOException{
-      LongFormQuestion round1 = new LongFormQuestion();
-      
-        //need to call in the answer string from the csv file
-        if(RoundOne.playerAnswer != round1.getAnswer()){
+
+    /**
+     * 
+     * @param playerAnswer
+     * @throws IOException 
+     */
+    public static void checkAnswer(String playerAnswer) throws IOException {
+        LongFormQuestion round1 = new LongFormQuestion();
+
+        if (RoundOne.playerAnswer == null ? round1.getAnswer() != null : !RoundOne.playerAnswer.equals(round1.getAnswer())) {
             System.out.println("Incorrect Answer!"
-                       + "The Answer was: ");
+                    + "The Answer was: " + round1.getAnswer());
             //next question
-           }
-        else if(RoundOne.playerAnswer.equals(round1.getAnswer())){
-           System.out.println("Correct!");
-           players.setPlayerCash(players.getPlayerCash()+1);
-           
-           //next question
-           }
+        } else if (RoundOne.playerAnswer.equals(round1.getAnswer())) {
+            System.out.println("Correct!");
+            players.setPlayerCash(players.getPlayerCash() + 1);
+        }
+        else{
+            System.out.println("Something went wrong...");
+        }
     }
-    
-    //interal timer stuff
-    Toolkit toolkit;
-
-    Timer timer;
-
+   
+/**
+ * 
+ * @param seconds 
+ */
     public RoundOne(int seconds) {
         toolkit = Toolkit.getDefaultToolkit();
         timer = new Timer();
         timer.schedule(new timeOut(), seconds * 1000);
     }
 
-    //calls the timer to end
+    /**
+     * 
+     */
     class timeOut extends TimerTask {
 
         public void run() {
             System.out.println("Time's up!");
+
             toolkit.beep();
             timer.cancel();
+
         }
     }
 
     //sets and prints the timer and questions
+    /**
+     * 
+     * @param args
+     * @throws IOException 
+     */
     public static void main(String args[]) throws IOException {
         System.out.println("Answer as many questions as you can in 1 minute! \n"
                 + "Timer starting;");
-        
+
         new RoundOne(60);
         System.out.println("Go!");
-        
+
         Scanner scan = new Scanner(System.in);
         //infinite method for printing questions, will end with the timer
-        for(int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             printQuestion();
             playerAnswer = scan.next();
             checkAnswer(RoundOne.playerAnswer);
         }
     }
-    
+
 }
-   
--- src/TheChase.java
+++ src/TheChase.java
@@ -5,68 +5,98 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
- * Abby - 19071317
- * Julia - 19078503
- * Matt - 19076935
+ * Abby - 19071317 Julia - 19078503 Matt - 19076935
  */
 public class TheChase {
 
-    /** The program entry point
+    /**
+     * The program entry point
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
         // Make this prettier to match the output style
         // Also make sure to add in the main menu before this
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Player name?");
-        String player = scanner.nextLine();
-        System.out.println("Chaser name?");
-        String chaser = scanner.nextLine();
-        Players players = new Players(chaser, player);
-        
-        System.out.println("\n");
-                
-        System.out.println(new LongFormQuestion());
-        
-        //ROUND ONE
-        RoundOne round1 = new RoundOne(" ", players);
-        //This should be set to the value from Round 1
-        WinMessage wm = new WinMessage(players.getPlayerCash(), 1);
-        wm.printWinRoundMessage();
 
+        System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+");
+        System.out.println("|            Welcome to              |");
+        System.out.println("|               the                  |");
+        System.out.println("|              Chase!                |");
+        System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n");
+
+        int selection;
+        Scanner scanner = new Scanner(System.in);
         
-        //END OF ROUND 1 GAMEPLAY
-        
-        //ROUND 2 GAMEPLAY
-        Round2 round2 = new Round2(players.getPlayerCash());
-        round2.playerOfferChoice();
-        round2.makeQuestionsList();
-        int wonLost = round2.startRound();
-        
-        switch(wonLost){
-            case 0:
-                //LoseMessage lm = new LoseMessage(??);
-                //lm.printLoseMessage();
-                System.out.println("Lose Message Here");
-                break;
-            case 1:
-                wm = new WinMessage(players.getPlayerCash(), 2);
-                wm.printWinRoundMessage();
-                break;
-            default:
-                System.out.println("Error in Round 2 return.");
-                break;
-        }
-        
-        players.setPlayerCash(round2.getMoneyRound2());
-        //END OF ROUND 2 GAMEPLAY
-        
-        
-        //ROUND 3 GAMEPLAY
-        
-        //END OF ROUND 3 GAMEPLAY
+        do {
+            System.out.println("Please Choose an Option: \n"
+                    + "1. Play The Chase \n"
+                    + "2. How to Play \n"
+                    + "3. Exit");
+            selection = scanner.nextInt();
+
+            switch (selection) {
+                case 1:
+                    //game starts
+                    
+                    System.out.println("Player name?");
+                    String player = scanner.nextLine();
+                    System.out.println("Chaser name?");
+                    String chaser = scanner.nextLine();
+                    Players players = new Players(chaser, player);
+
+                    System.out.println("\n");
+
+                    System.out.println(new LongFormQuestion());
+
+                    //ROUND ONE
+                    RoundOne round1 = new RoundOne(" ", players);
+                    //This should be set to the value from Round 1
+
+                    WinMessage wm = new WinMessage(players.getPlayerCash(), 1);
+                    wm.printWinRoundMessage();
+
+                    //END OF ROUND 1 GAMEPLAY
+                    //ROUND 2 GAMEPLAY
+                    Round2 round2 = new Round2(players.getPlayerCash());
+                    round2.playerOfferChoice();
+                    round2.makeQuestionsList();
+                    int wonLost = round2.startRound();
+
+                    switch (wonLost) {
+                        case 0:
+                            //LoseMessage lm = new LoseMessage(??);
+                            //lm.printLoseMessage();
+                            System.out.println("Lose Message Here");
+                            break;
+                        case 1:
+                            wm = new WinMessage(players.getPlayerCash(), 2);
+                            wm.printWinRoundMessage();
+                            break;
+                        default:
+                            System.out.println("Error in Round 2 return.");
+                            break;
+                    }
+
+                    players.setPlayerCash(round2.getMoneyRound2());
+                    //END OF ROUND 2 GAMEPLAY
+
+                    //ROUND 3 GAMEPLAY
+                    //END OF ROUND 3 GAMEPLAY
+                    break;
+                case 2:
+                    System.out.println("Game instructions: \n"
+                            + "This game has been modled of the popular quiz game show 'The Chase'. \n"
+                            + "To play: enter the name of the player and then the chaser - this is a two player\n"
+                            + "game. Then follow the propmts and either try to out chase the chaser or catch the player!");
+                    break;
+                case 3:
+                    System.out.println("Goodbye!");
+                    System.exit(0);
+                    break;
+                default:
+                    throw new IndexOutOfBoundsException();
+            }
+
+        } while (selection == 1);
     }
-    
-    
-    
 }
