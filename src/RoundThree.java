d4cf2f119ecaa9bcc759d592f7daeb31664a88fa
zqj5293 <80650732+zqj5293@users.noreply.github.com>
2021-04-11 17:54:29 +1200

    Update RoundThree.java

--- src/RoundThree.java
+++ src/RoundThree.java
@@ -3,16 +3,7 @@ import java.awt.Toolkit;
 import java.util.Timer;
 import java.util.TimerTask;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author julia
- */
 public class RoundThree {
     
     Toolkit toolkit;
